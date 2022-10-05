540238879e3430d8bac28d2bb8ccb4a292dea6c9
Jean <jean_luccalp@hotmail.com>
2022-10-05 17:46:20 +0200

    adicionando o jquery

---
+++ script.js
@@ -0,0 +1,25 @@
+// $function() {
+//     $(".br").click() => {
+//         $(".sobre").text("teste");
+//         $(".sobre").text("Desenvolvedor Front-End");
+//         // $(".language").text("br");
+//         $("#titulo").text("Não sei se está funcionando!");
+//         // $(".language-selected").addClass(".change-br");
+//     }
+// };
+
+// $function() {
+//     $(".it").click() => {
+//         $("h2").css({'color': 'red'});  
+//         $(".sobre").text("Front-End development");
+//         $("#titulo").text("No lo so si sta funcionando");
+//     }
+// };
+
+// $function() {
+//     $(".en").click() => {
+//         $(".sobre").text("teste");
+//         $(".sobre").text("Sviluppatore Front-End");
+//     }
+// };
+
