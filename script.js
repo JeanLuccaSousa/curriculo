139161ad9d0a47a42f9954d2f8bb26b823053cb2
Jean <jean_luccalp@hotmail.com>
2022-10-12 17:48:01 +0200

    Adicionando o jquery

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-// $function() {
-//     $(".br").click() => {
-//         $(".sobre").text("teste");
-//         $(".sobre").text("Desenvolvedor Front-End");
-//         // $(".language").text("br");
-//         $("#titulo").text("Não sei se está funcionando!");
-//         // $(".language-selected").addClass(".change-br");
-//     }
-// };
+// Portoghese 
+$(document).ready(()=> { 
+    $('.pt').click(function() {
+        $('.sobre').text('Sobre mim')
+        $('.sobre').text('Desenvolvedor Front-End')
+        $('.ferramentas_trabalho').text('Principais ferramentas de trabalho')
+        $('.botoes').text('Baixar CV')
+        $('.botoes').text('Entre em contato')
+        $('.rodape').text('Desenvolvido por JeanLuccaSousa')
+    });
+}) 
 
-// $function() {
-//     $(".it").click() => {
-//         $("h2").css({'color': 'red'});  
-//         $(".sobre").text("Front-End development");
-//         $("#titulo").text("No lo so si sta funcionando");
-//     }
-// };
+ // Italian 
+$(document).ready(()=> { 
+    $('.it').click(function() {
+        $('.sobre').text('Un po di me')
+        $('.sobre').text('Sviluppatore Front-End')
+        $('.ferramentas_trabalho').text('Principali strumenti di lavoro')
+        $('.botoes1').text('Scarica CV')
+        $('.botoes2').text('Contatto')
+        $('.rodape').text('Sviluppato da JeanLuccaSousa')
+    });
+}) 
 
-// $function() {
-//     $(".en").click() => {
-//         $(".sobre").text("teste");
-//         $(".sobre").text("Sviluppatore Front-End");
-//     }
-// };
-
+// English
+$(document).ready(()=> { 
+    $('.en').click(function() {
+        $('.sobre').text('About me')
+        $('.sobre').text('Front-End developer')
+        $('.ferramentas_trabalho').text('Main work tools')
+        $('.botoes').text('Download CV')
+        $('.botoes').text('Contact')
+        $('.rodape').text('Developed by JeanLuccaSousa')
+    });
+}) 
