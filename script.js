bca0213cedb14268177b482b3e53a7ddc0c856e4
Jean <jean_luccalp@hotmail.com>
2022-11-03 17:55:42 +0100

    adicionando as bandeiras

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Portoghese 
 $(document).ready(()=> { 
     $('.pt').click(function() {
+        $('.language-selected').text('Português-Brasil')
+        $('.language-selected').addClass('.change-pt')
         $('.titulo1').text('Sobre mim')
         $('.paragrafo1').text('Desenvolvedor Front-End')
         $('.titulo2').text('Principais ferramentas de trabalho')
@@ -14,6 +16,8 @@ $(document).ready(()=> {
 $(document).ready(()=> { 
     $('.it').click(function() {
         $('.titulo1').text('Un po di me')
+        $('.language').addClass('change.it')
+        $('.language-selected').text('Italiano')
         $('.paragrafo1').text('Sviluppatore Front-End')
         $('.titulo2').text('Principali strumenti di lavoro')
         $('.btn1').text('Scarica CV')
@@ -27,6 +31,8 @@ $(document).ready(()=> {
 $(document).ready(()=> { 
     $('.en').click(function() {
         $('.titulo1').text('About me')
+        $('.language-selected').text('English')
+        $('.language-selected').addClass('.change-en')
         $('.paragrafo1').text('Front-End developer')
         $('.titulo2').text('Main work tools')
         $('.btn1').text('Download CV')
