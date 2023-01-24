e8988f2931df4ce88be1d02864953ba76b52185a
Jean <jean_luccalp@hotmail.com>
2023-01-24 18:39:47 +0100

    continuação

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ $(document).ready(()=> {
         $('.titulo1').text('Sobre mim')
         $('.language-selected').text('Português')
         $('.language-selected').addClass('change-pt')
-      
         $('.titulo1').text('Sobre mim')
-        $('.paragrafo1').text('Desenvolvedor Front-End')
+        $('.paragrafo1').text('Desenvolvedor Web apaixonado por tecnologia e em constante aprendizado.')
         $('.titulo2').text('Principais ferramentas de trabalho')
         $('.btn1').text('Baixar CV')
         $('.btn2').text('Entre em contato')
@@ -20,8 +19,7 @@ $(document).ready(()=> {
         $('.titulo1').text('Un po di me')
         $('.language-selected').text('Italiano')
         $('.language-selected').addClass('change-it')
-      
-        $('.paragrafo1').text('Sviluppatore Front-End')
+        $('.paragrafo1').text('Web developer appassionato di tecnologia e in costante apprendimento.')
         $('.titulo2').text('Principali strumenti di lavoro')
         $('.btn1').text('Scarica CV')
         $('.btn2').text('Contatto')
@@ -29,16 +27,13 @@ $(document).ready(()=> {
     });
 }) 
 
-
 // English
 $(document).ready(()=> { 
-    
     $('.en').click(function() {
         $('.titulo1').text('About me')
         $('.language-selected').text('English')
         $('.language-selected').addClass('change-en')
-       
-        $('.paragrafo1').text('Front-End developer')
+        $('.paragrafo1').text('Web developer passionate about technology and in constant learning.')
         $('.titulo2').text('Main work tools')
         $('.btn1').text('Download CV')
         $('.btn2').text('Contact')
