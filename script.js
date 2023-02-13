ed439253418bb0c7b4eb2aba16aed433b656b058
Jean <jean_luccalp@hotmail.com>
2023-02-13 18:27:20 +0100

    Finalização do Projeto

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Portoghese 
 $(document).ready(()=> { 
     $('.pt').click(function() {
+        $('.titulo_inicio').text('Sobre mim')
         $('.titulo1').text('Sobre mim')
         $('.language-selected').text('Português')
         $('.language-selected').addClass('change-pt')
         $('.titulo1').text('Sobre mim')
-        $('.paragrafo1').text('Desenvolvedor Web apaixonado por tecnologia e em constante aprendizado.')
+        $('.paragrafo1').text('Olá, meu nome é Jean Lucca de Sousa!')
+        $('.paragrafo2').text('E este é meu Portfólio')
+        $('.botao_github').text('Meu GitHub')
+        $('.sobre-mim').text('Sobre mim')
+        $('.sobre-paragrafo').text('Me chamo Jean e sou desenvolvedor web júnior e trabalho com desenvolvimento Front-End. Desenvolvi minha paixão por programação quando, por curiosidade, comecei a criar páginas da web estudando sozinho. As minhas principais competências são: Organização, aptidão para trabalho em equipe, flexibilidade e responsabilidade.')
+        $('.habilidades').text('Habilidades:')
+        $('.lista1').text('Conhecimento das principais linguagens de programação: HTML, CSS, JAVASCRIPT, JQUERY, BOOTSTRAP e WORDPRESS;')
+        $('.lista2').text('Conhecimento básico de Java e PHP;')
+        $('.lista3').text('Conhecimento básico de Banco de Dados MySQL;')
+        $('.lista4').text('Conhecimento do repositório git(GitHub, Gitlab, etc);')
+        $('.lista5').text('VsCode e NetBeans.')
         $('.titulo2').text('Principais ferramentas de trabalho')
-        $('.btn1').text('Baixar CV')
-        $('.btn2').text('Entre em contato')
+        $('.contato').text('Meus contatos')
+        $('.botao_linkedin').text('Meu linkedin')
         $('.rodape').text('Desenvolvido por Jean Lucca De Sousa')
     });
 }) 
@@ -16,13 +27,24 @@ $(document).ready(()=> {
  // Italian 
 $(document).ready(()=> { 
     $('.it').click(function() {
+        $('.titulo_inicio').text('Un po di me')
         $('.titulo1').text('Un po di me')
         $('.language-selected').text('Italiano')
         $('.language-selected').addClass('change-it')
-        $('.paragrafo1').text('Web developer appassionato di tecnologia e in costante apprendimento.')
+        $('.paragrafo1').text('Ciao, mi chiamo Jean Lucca de Sousa!')
+        $('.paragrafo2').text('E questo è il mio Portafoglio')
+        $('.botao_github').text('Mio GitHub')
+        $('.sobre-mim').text('Un po di me')
+        $('.sobre-paragrafo').text("Mi chiamo Jean e sono uno sviluppatore web junior e lavoro con sviluppo Front-End. Ho maturato la mia passione per la programmazione quando per curiosità ho iniziato a realizzare pagine web studiando da autodidatta. Le mie principali competenze in quest'ambito sono: Organizzazione, attitudine al lavoro in team, flessibilità e responsabilità.")
+        $('.habilidades').text('Skills:')
+        $('.lista1').text('Conoscenza dei principali linguagi web: HTML, CSS, JAVASCRIPT, JQUERY, BOOTSTRAP e WORDPRESS;')
+        $('.lista2').text('Conoscenza di base di Java e PHP;')
+        $('.lista3').text('Conoscenza di base di database MySQL;')
+        $('.lista4').text("Conoscenza di base dell'uso delle repository git(GitHub, Gitlab, ecc);")
+        $('.lista5').text('VsCode e NetBeans.')
         $('.titulo2').text('Principali strumenti di lavoro')
-        $('.btn1').text('Scarica CV')
-        $('.btn2').text('Contatto')
+        $('.contato').text('Miei contatti')
+        $('.botao_linkedin').text('Mio linkedin')
         $('.rodape').text('Sviluppato da Jean Lucca De Sousa')
     });
 }) 
@@ -30,16 +52,30 @@ $(document).ready(()=> {
 // English
 $(document).ready(()=> { 
     $('.en').click(function() {
+        $('.titulo_inicio').text('About me')
         $('.titulo1').text('About me')
         $('.language-selected').text('English')
         $('.language-selected').addClass('change-en')
-        $('.paragrafo1').text('Web developer passionate about technology and in constant learning.')
+        $('.paragrafo1').text('Hi, my name is Jean Lucca de Sousa!')
+        $('.paragrafo2').text('And this is my Portfolio')
+        $('.botao_github').text('My GitHub')
+        $('.sobre-mim').text('About me')
+        $('.sobre-paragrafo').text("My name is Jean and I'm a junior web developer and I work with Front-End development. I developed my passion for programming when, out of curiosity, I started creating web pages by studying on my own. My main skills are: Organization, aptitude for teamwork, flexibility and responsibility.")
+        $('.habilidades').text('Skills:')
+        $('.lista1').text('Knowledge of the main web languages: HTML, CSS, JAVASCRIPT, JQUERY, BOOTSTRAP e WORDPRESS;')
+        $('.lista2').text('Basic knowledge of Java and PHP;')
+        $('.lista3').text('Basic knowledge of MySQL databases;')
+        $('.lista4').text('Basic knowledge of using git repositories (GitHub, Gitlab, etc);')
+        $('.lista5').text('VsCode e NetBeans.')
         $('.titulo2').text('Main work tools')
-        $('.btn1').text('Download CV')
-        $('.btn2').text('Contact')
+        $('.contato').text('My contacts')
+        $('.botao_linkedin').text('My linkedin')
         $('.rodape').text('Developed by Jean Lucca De Sousa')
     });
-
-    var teste = document.getElementById("git");
-    teste.innerHTML = 'target="blank_"'
 }) 
+
+
+
+
+
+
